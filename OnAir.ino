7b92eeb9891f2ce3889d9aff4bdb0b6a78c4a895
Raphael Pour <info@raphaelpour.de>
2020-04-10 14:13:04 +0200

    Update OnAir.ino

--- OnAir.ino
+++ OnAir.ino
@@ -1,9 +1,13 @@
 /*
- * On Air - Voice Chat Push to talk keyboard
+ * @file:      OnAir.ino
+ * @brief:     Push-to-talk keyboard
+ * @date:      Spring 2020
+ * @author:    Raphael Pour <dev@raphaelpour.de>
+ * @copyright: (C) 2020 Raphael Pour licensed under the GPLv2
  */
 #include <Keyboard.h>
 
-/* Push to talk key combination which should be fired */
+/* Hotkey which should be pressed*/
 char key_combination[] = 
 {
   KEY_LEFT_CTRL,
@@ -13,9 +17,19 @@ char key_combination[] =
 /*
  * Pin mapping
  */
+
+/* Input pin connected to the pushbutton */
 #define PUSH_TO_TALK_INPUT 2
+
+/* Input pin connected to the switch */
 #define SWITCH_TO_TALK_INPUT 9
+
+/* Output pin connected to the Red LED */
 #define LED_PIN 10
+
+/*
+ * Blink State
+ */
 
 bool blinkState = 0;
 unsigned long nextBlink = 0;
@@ -26,13 +40,27 @@ unsigned long nextBlink = 0;
  */
 int onAir = false;
 
+/**
+ * setup:
+ * Prepares the Pins and starts the initializes the Keyboard.
+ * After the setup, the connected PC should detect the
+ * Arduino as Keyboard/HID (Human Interface Device).
+ */
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(PUSH_TO_TALK_INPUT, INPUT);
   Keyboard.begin();
 }
 
-
+/**
+ * goLive:
+ * Activate push-to-talk once by sending the defined hotkey as
+ * 'key press' to the connected PC.
+ *
+ * Once means, that the key press won't be sent multiple times
+ * to not confuse the key-handling of the program that will
+ * receive the keys at the PC.
+ */
 void goLive()
 {
   if(!onAir)
@@ -44,6 +72,13 @@ void goLive()
   onAir = true;
 }
 
+/**
+ * goSilent:
+ * Deactivates push-to-talk by sending a release as 
+ * 'key release all' to the connected PC.
+ *
+ * It also cares about turning off the LED.
+ */
 void goSilent()
 {
   if(onAir) Keyboard.releaseAll();  
@@ -51,6 +86,12 @@ void goSilent()
   onAir = false;
 }
 
+/**
+ * blinkEvent:
+ * Let the LED blink. This function should be called periodically
+ * when push-to-talk is activated. It automatically checks when
+ * it is time to turn the LED on/off.
+ */
 void blinkEvent()
 {
   if(millis() < nextBlink) return;
@@ -61,6 +102,14 @@ void blinkEvent()
 
   blinkState = !blinkState;
 }
+
+/**
+ * loop:
+ * Main loop where all Inputs will be evaluated and checked if
+ * push-to-talk should be enabled/disabled.
+ * It also calls the blinkEvent periodically when push-to-talk
+ * is enabled.
+ */
 void loop() {
   
   if(digitalRead(SWITCH_TO_TALK_INPUT) == HIGH ||
